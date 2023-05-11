025a4a6b32b538a17db5945c2649f48ca514e093
FranciscaOrtegaG <franciscaortega713@gmail.com>
2023-05-11 16:09:05 -0400

    added: css and html linked to mapa apk

--- foodproject/mapa/views.py
+++ foodproject/mapa/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def mapa(request):
-    return render(request, 'mapa.html')
+    return render(request, './mapa.html')
 
 # Create your views here.
+
