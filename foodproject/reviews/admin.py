f4f13263b2ac7e666049e3ee01a40bd16320cc36
Lucas-CE <lucas.carrascoestay@gmail.com>
2023-05-13 00:15:20 -0400

    feat: En admin.py se registran los modelos de Puesto_de_comida y de Evaluacion

--- foodproject/reviews/admin.py
+++ foodproject/reviews/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from reviews.models import User
+from reviews.models import User, Puesto_de_comida, Evaluacion
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Puesto_de_comida)
+admin.site.register(Evaluacion)
