f3fafd23cdd2bc7d40959655df422521af2f577b
Lucas-CE <lucas.carrascoestay@gmail.com>
2023-05-14 10:22:36 -0400

    fix: Se arregla el nombre del campo usado para el puesto de comida y agrega usuario al form temporalmente, solo para testeo

--- foodproject/reviews/forms.py
+++ foodproject/reviews/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from reviews.models import Puesto_de_comida
+from reviews.models import Puesto_de_comida, User
 
 class NewUserForm(forms.Form):
    
@@ -11,7 +11,8 @@ class CrearReseñaForm(forms.Form):
    calificacion_precio = forms.IntegerField(max_value=5, min_value=1)
    calificacion_presentacion = forms.IntegerField(max_value=5, min_value=1)
    comentario = forms.CharField(widget=forms.Textarea()) # <textarea> en vez de <input>
-   puesto_de_comida = forms.ModelChoiceField(queryset=Puesto_de_comida.objects.all())
+   local_comida = forms.ModelChoiceField(queryset=Puesto_de_comida.objects.all())
+   usuario = forms.ModelChoiceField(queryset=User.objects.all()) #temporalmente, solo para testeo
 
 
 
