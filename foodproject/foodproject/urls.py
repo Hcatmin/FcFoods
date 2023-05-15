b603e486c7e237f778c0358ab3d86dd23e3dc86e
Roberto Alvarado <roberto.alvarado@ug.uchile.cl>
2023-05-14 21:43:51 -0400

    fix: URL path

--- foodproject/foodproject/urls.py
+++ foodproject/foodproject/urls.py
@@ -21,7 +21,7 @@ from reviews.views import home, perfil, register_user, login_request, lista_de_r
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home, name="home"),
-    path('profile/', perfil),
+    path('profile/', perfil, name="profile"),
     path('register', register_user, name='register_user'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('mapa/', include ('mapa.urls'), name='mapa'),
