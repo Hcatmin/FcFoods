27715b20af352fd46eefc10a41739f727944ed52
Lucas-CE <lucas.carrascoestay@gmail.com>
2023-08-01 23:12:22 -0400

    style: :art: Formatting spaces in python files

--- foodproject/reviews/forms.py
+++ foodproject/reviews/forms.py
@@ -1,30 +1,39 @@
 from django import forms
 from reviews.models import Puesto_de_comida, User, Evaluacion, Comentario
 
+
 class NewUserForm(forms.Form):
-   
-   email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True)
 
 
 class CrearReseñaForm(forms.ModelForm):
-   CHOICES = [(5, '☆'), (4, '☆'), (3, '☆'), (2, '☆'), (1, '☆')]
-   calificacion_comida = forms.ChoiceField(widget=forms.RadioSelect(), choices=CHOICES)
-   calificacion_precio = forms.ChoiceField(widget=forms.RadioSelect(), choices=CHOICES)
-   calificacion_presentacion = forms.ChoiceField(widget=forms.RadioSelect(), choices=CHOICES)
-   detalle = forms.CharField(widget=forms.Textarea(attrs={'rows':'4'}), required=False) # <textarea> en vez de <input>
+    CHOICES = [(5, "☆"), (4, "☆"), (3, "☆"), (2, "☆"), (1, "☆")]
+    calificacion_comida = forms.ChoiceField(widget=forms.RadioSelect(), choices=CHOICES)
+    calificacion_precio = forms.ChoiceField(widget=forms.RadioSelect(), choices=CHOICES)
+    calificacion_presentacion = forms.ChoiceField(
+        widget=forms.RadioSelect(), choices=CHOICES
+    )
+    detalle = forms.CharField(
+        widget=forms.Textarea(attrs={"rows": "4"}), required=False
+    )  # <textarea> en vez de <input>
 
-   class Meta:
-      model = Evaluacion
-      fields = ["calificacion_comida", "calificacion_precio", "calificacion_presentacion", "detalle"]
+    class Meta:
+        model = Evaluacion
+        fields = [
+            "calificacion_comida",
+            "calificacion_precio",
+            "calificacion_presentacion",
+            "detalle",
+        ]
+
 
 class SearchForm(forms.Form):
-   busqueda = forms.CharField(required=True)
+    busqueda = forms.CharField(required=True)
 
 
 class ComentarioReseña(forms.ModelForm):
-   comentario = forms.CharField(required=True) # <textarea> en vez de <input>
-   class Meta:
-      model = Comentario
-      fields = ['comentario']
+    comentario = forms.CharField(required=True)  # <textarea> en vez de <input>
 
-
+    class Meta:
+        model = Comentario
+        fields = ["comentario"]
--- foodproject/reviews/models.py
+++ foodproject/reviews/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-#from django.contrib.auth.models import User
+
+# from django.contrib.auth.models import User
 
 
 from django.contrib.auth.models import AbstractUser
+
 
 # Modelo que representa a los usuarios
 # Posee de atributos: username: el nombre de usuario
@@ -10,8 +12,9 @@ from django.contrib.auth.models import AbstractUser
 #                     pronombre: el pronombre del usuario
 #                     contraseña: la contraseña del usuario
 class User(AbstractUser):
-  pronombres = [('La','La'),('El','El'), ('Le','Le'),('Otro','Otro')]
-  pronombre = models.CharField(max_length=5,choices=pronombres)
+    pronombres = [("La", "La"), ("El", "El"), ("Le", "Le"), ("Otro", "Otro")]
+    pronombre = models.CharField(max_length=5, choices=pronombres)
+
 
 # Modelo que representa los puestos de comida que se instalan fuera de 850
 # Posee de atributos: nombre: el nombre del puesto de comida
@@ -20,13 +23,14 @@ class User(AbstractUser):
 class Puesto_de_comida(models.Model):
     nombre = models.CharField(max_length=200)
     dueño = models.CharField(max_length=200, blank=True)
-    FotoLocal = models.ImageField(upload_to='Fotos_locales/', null=True, blank=True)
+    FotoLocal = models.ImageField(upload_to="Fotos_locales/", null=True, blank=True)
 
     def __str__(self):
         return self.nombre
 
+
 # Modelo que representa las reseñas de los puestos de comida
-# Posee de atributos: usuario: el usuario que creo la reseña 
+# Posee de atributos: usuario: el usuario que creo la reseña
 #                     local_comida: el puesto de comida del que se está haciendo la reseña
 #                     detalle: un texto adicional que se le puede agregar a la reseña
 #                     calificacion_comida: un puntaje entre 1 y 5 que se le da a la comida
@@ -39,22 +43,28 @@ class Puesto_de_comida(models.Model):
 #                     usuario_dio_dislike: usuarios que dieron dislike a la reseña
 class Evaluacion(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    local_comida = models.ForeignKey(Puesto_de_comida, related_name='evaluaciones', on_delete=models.CASCADE)
+    local_comida = models.ForeignKey(
+        Puesto_de_comida, related_name="evaluaciones", on_delete=models.CASCADE
+    )
     detalle = models.TextField(blank=True)
     calificacion_comida = models.IntegerField()
     calificacion_precio = models.IntegerField()
     calificacion_presentacion = models.IntegerField()
     fecha = models.DateTimeField(auto_now_add=True)
-    usuario_dio_like = models.ManyToManyField(User, related_name='like_reviews', blank=True)
-    usuario_dio_dislike = models.ManyToManyField(User, related_name='dislike_reviews', blank=True)
-    
+    usuario_dio_like = models.ManyToManyField(
+        User, related_name="like_reviews", blank=True
+    )
+    usuario_dio_dislike = models.ManyToManyField(
+        User, related_name="dislike_reviews", blank=True
+    )
+
     def _str_(self):
         return f"{self.usuario.username} evaluó {self.local_comida.nombre}"
-    
+
     def dar_like(self, usuario):
         self.usuario_dio_like.add(usuario)
         self.save()
-    
+
     def dar_dislike(self, usuario):
         self.usuario_dio_dislike.add(usuario)
         self.save()
@@ -65,22 +75,26 @@ class Evaluacion(models.Model):
     # método para saber si un usuario ha dado dislike a una reseña
     def confirmacion_usuario_disliked(self, user):
         return self.usuario_dio_dislike.filter(id=user.id).exists()
-    
+
     def cantidad_likes(self):
         return self.usuario_dio_like.count()
-    
+
     def cantidad_dislikes(self):
         return self.usuario_dio_dislike.count()
-#Modelo que representa el comentario que se le da a una reseña    
-#Atributos : evaluación: la evaluación que está comentada
-#            comentarista: el usuario calificante, que es quien califica la reseña     
+
+
+# Modelo que representa el comentario que se le da a una reseña
+# Atributos : evaluación: la evaluación que está comentada
+#            comentarista: el usuario calificante, que es quien califica la reseña
 #            comentario: comentario a la reseña
 #            es_util: si el comentario es útil o no
 #            like_comentario: cantidad de likes que tiene el comentario
 #            dislike_comentario: cantidad de dislikes que tiene el comentario
 #            fechacomentario: fecha en la que se hizo el comentario
 class Comentario(models.Model):
-    evaluacion = models.ForeignKey(Evaluacion, related_name='calificaciones', on_delete=models.CASCADE)
+    evaluacion = models.ForeignKey(
+        Evaluacion, related_name="calificaciones", on_delete=models.CASCADE
+    )
     comentarista = models.ForeignKey(User, on_delete=models.CASCADE)
     comentario = models.TextField(blank=False)
     es_util = models.BooleanField(default=False)
@@ -89,8 +103,8 @@ class Comentario(models.Model):
     fechacomentario = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-      return f"{self.comentarista.username} comentó la evaluación de {self.evaluacion.usuario.username}"
-    
+        return f"{self.comentarista.username} comentó la evaluación de {self.evaluacion.usuario.username}"
+
     def valorizar_util(self):
         self.es_util = True
         self.save()
@@ -114,4 +128,3 @@ class Comentario(models.Model):
     def quitar_dislikeComentario(self):
         self.dislike_comentario -= 1
         self.save()
-        
